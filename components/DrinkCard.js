ce28548227d70b261e9c670f9a3e826e1ebe598a
Jay Pedersen <jay@jaybuddy.com>
2019-02-23 10:09:36 -0800

    add ingredients to card

--- components/DrinkCard.js
+++ components/DrinkCard.js
@@ -19,6 +19,13 @@ const DrinkCard = (props) => {
     border-top: 1px solid #CCCCCC;
   `;
 
+  const Ingredients = styled.div`
+    display: block;
+    margin-top: 10px;
+    padding-top: 5px;
+    border-top: 1px solid #CCCCCC;
+  `;
+
   const SmallText = styled.span`
     font-size: 12px;
     color: #515151;
@@ -33,6 +40,12 @@ const DrinkCard = (props) => {
             <Heading>{props.drink.name}</Heading>
           </Card.Header>
           <Card.Body>
+            <Ingredients>
+              <SmallText>Ingredients:</SmallText> <br />
+              {(props.drink.Ingredients && props.drink.Ingredients.length > 0)
+                ? props.drink.Ingredients.map(ingredient => <Ingredient key={ingredient._id} ingredient={ingredient} />)
+                : <SmallText>None</SmallText>}
+            </Ingredients>
             <Badges>
               <SmallText>Tags:</SmallText> <br />
               {(props.drink.Tags && props.drink.Tags.length > 0)
@@ -54,6 +67,17 @@ const Tag = props => <Badge variant="primary">{props.tag.name}</Badge>;
 
 Tag.propTypes = {
   tag: PropTypes.object,
+};
+
+const Ingredient = props => (
+  <div>
+    {props.ingredient.name} <br/>
+    {props.ingredient.notes}
+  </div>
+);
+
+Ingredient.propTypes = {
+  ingredient: PropTypes.object,
 };
 
 export default DrinkCard;
--- components/containers/BookContainer.js
+++ components/containers/BookContainer.js
@@ -2,9 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import DrinkCard from '../DrinkCard';
 
-const BookContainer = (props) => {
-  return props.drinks ? props.drinks.map(drink => <DrinkCard key={drink._id} drink={drink} />) : 'loading';
-}
+const BookContainer = (props) => props.drinks ? props.drinks.map(drink => <DrinkCard key={drink._id} drink={drink} />) : 'loading';
 
 BookContainer.propTypes = {
   drinks: PropTypes.array,
